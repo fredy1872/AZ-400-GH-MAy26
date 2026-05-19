ae71310875160d5a6d1049bda3910b7819e274ff
John Doe <johndoe@gmail.com>
2026-05-19 18:10:53 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AZ_400_May_26_APP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
